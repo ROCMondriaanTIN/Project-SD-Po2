e9b1f2c56a975688d7e42f91dbb8688e4b19860e
KryonX <bartekjaworski416@gmail.com>
2025-12-12 16:52:45 +0100

    win kans verhoogt en big win en maga win toe ge voegd

--- slots2.js
+++ slots2.js
@@ -1,5 +1,5 @@
 let Money = 1000;
-let WinRate = [34, 95, 11, 77, 124];
+let WinRate = [17, 18, 19, 20, 21];
 let bet = 5;
 
 // screen cast
@@ -15,7 +15,7 @@ function Placebet(){
 function play() {
     if (Money < bet) { alert("Niet genoeg saldo!"); return }
     SpinMachine()
-
+    showSpin()
 }
 
 // additionalcode
@@ -34,34 +34,45 @@ function SpinMachine(){
     Money -= bet;
     NumberGeneration()
 
-    if(WinNum === 140){
-        console.log("jackpot!!!");
-        Money += bet*10000 + bet;
-        console.log(Money)
-
-    } else {
-        let isWinningNumber = false
-
-        for (let i = 0; i < WinRate.length; i++) {
-            if( WinNum === WinRate[i] ) 
-                isWinningNumber = true
-        }
-
-        if( isWinningNumber ){
-            console.log("win");
-            Money += bet*6;
-            console.log(Money)
-        }else{
-            console.log("loser");
+    if(mageWin === 10000){
+        console.log("Mega Jackpot!!!");
+        Money += bet*5000 + bet;
+        console.log(Money);
+    }else{
+        if(bigWin === 2000){
+            console.log("jackpot!!!");
+            Money += bet*1000 + bet;
             console.log(Money);
+
+        }else{
+            let isWinningNumber = false
+
+            for (let i = 0; i < WinRate.length; i++) {
+                if( WinNum === WinRate[i] ) 
+                    isWinningNumber = true
+            }
+
+            if( isWinningNumber ){
+                console.log("win");
+                Money += bet*2 + bet;
+                console.log(Money);
+            }else{
+                console.log("loser");
+                console.log(Money);
+            }
         }
     }
 }
 
 function NumberGeneration(){
-    Num1 = Math.floor(Math.random() * 35) + 1;
-    Num2 = Math.floor(Math.random() * 35) + 1;
-    Num3 = Math.floor(Math.random() * 35) + 1;
-    Num4 = Math.floor(Math.random() * 35) + 1;
-    WinNum = Num1 + Num2 + Num3 + Num4;
+    Num1 = Math.floor(Math.random() * 20) + 1;
+    Num2 = Math.floor(Math.random() * 20) + 1;
+    WinNum = Num1 + Num2;
+
+    bigWin = Math.floor(Math.random() * 2000) + 1;
+    mageWin = Math.floor(Math.random() * 10000) + 1;
+}
+
+function showSpin(){
+
 }
